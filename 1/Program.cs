88835158a1e8196519c03e45ed816805664c9e74
Pedro Lopes <pedro.hsl2003@gmail.com>
2022-12-27 21:46:26 -0800

    Adicionar arquivos de projeto.

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+
+            string numero = Console.ReadLine();
+
+            Console.WriteLine("O número que você digitou foi " + numero);
+
+            Console.ReadKey();
+        }
+    }
+}
